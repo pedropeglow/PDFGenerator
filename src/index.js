10fa51c44695e94d8edb305d615f18f15281049e
pedropeglow <pedropeglowm@gmail.com>
2023-02-07 14:42:49 -0300

    :sparkles: Adicionando variáveis de ambiente.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import ReactGA from "react-ga";
+
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
+const { REACT_APP_TRACKINGID } = process.env;
+
+// Inicializando o Google Analytics
+if (process.env.REACT_APP_TRACKINGID != null) {
+  ReactGA.initialize(REACT_APP_TRACKINGID, {
+    cookieDomain: "auto",
+  });
+
+  history.listen((location) => {
+    ReactGA.set({ page: window.location.href }); // Update the user's current page
+    ReactGA.pageview(window.location.href); // Record a pageview for the given page
+  });
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
